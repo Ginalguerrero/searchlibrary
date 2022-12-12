e7a4364e66712e1208c0f380b3085e1d5411e5b0
ginaguerrero25 <gina.l.guerrero@gmail.com>
2022-12-12 10:36:52 -0800

    make pages

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,103 +1,95 @@
-import React, { useState, useEffect } from 'react';
-import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
-
-import { getMe, deleteBook } from '../utils/API';
+import React from 'react';
+import {
+	Jumbotron,
+	Container,
+	CardColumns,
+	Card,
+	Button,
+} from 'react-bootstrap';
+import { DELETE_BOOK } from '../utils/mutations';
+import { QUERY_ME } from '../utils/queries';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
-
+import { useQuery, useMutation } from '@apollo/client';
+import LinesEllipsis from 'react-lines-ellipsis'
+import Image from 'react-bootstrap/Image'
 const SavedBooks = () => {
-  const [userData, setUserData] = useState({});
+	const { loading, data } = useQuery(QUERY_ME, {fetchPolicy: "no-cache"});
+	const savedBooks = data?.me.savedBooks || [];
 
-  // use this to determine if `useEffect()` hook needs to run again
-  const userDataLength = Object.keys(userData).length;
+	const [deleteBook] = useMutation(DELETE_BOOK);
+	const handleDeleteBook = async (bookId) => {
+		const token = Auth.loggedIn() ? Auth.getToken() : null;
+		if (!token) {
+			return false;
+		}
 
-  useEffect(() => {
-    const getUserData = async () => {
-      try {
-        const token = Auth.loggedIn() ? Auth.getToken() : null;
+		try {
+			await deleteBook({
+				variables: { bookId },
+			});
+			removeBookId(bookId);
+			window.location.reload();
+		} catch (err) {
+			console.error(err);
+		}
+	};
 
-        if (!token) {
-          return false;
-        }
+	if (loading) {
+		return <h2>LOADING...</h2>;
+	}
 
-        const response = await getMe(token);
-
-        if (!response.ok) {
-          throw new Error('something went wrong!');
-        }
-
-        const user = await response.json();
-        setUserData(user);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-
-    getUserData();
-  }, [userDataLength]);
-
-  // create function that accepts the book's mongo _id value as param and deletes the book from the database
-  const handleDeleteBook = async (bookId) => {
-    const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-    if (!token) {
-      return false;
-    }
-
-    try {
-      const response = await deleteBook(bookId, token);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const updatedUser = await response.json();
-      setUserData(updatedUser);
-      // upon success, remove book's id from localStorage
-      removeBookId(bookId);
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
-  // if data isn't here yet, say so
-  if (!userDataLength) {
-    return <h2>LOADING...</h2>;
-  }
-
-  return (
-    <>
-      <Jumbotron fluid className='text-light bg-dark'>
-        <Container>
-          <h1>Viewing saved books!</h1>
-        </Container>
-      </Jumbotron>
-      <Container>
-        <h2>
-          {userData.savedBooks.length
-            ? `Viewing ${userData.savedBooks.length} saved ${userData.savedBooks.length === 1 ? 'book' : 'books'}:`
-            : 'You have no saved books!'}
-        </h2>
-        <CardColumns>
-          {userData.savedBooks.map((book) => {
-            return (
-              <Card key={book.bookId} border='dark'>
-                {book.image ? <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' /> : null}
-                <Card.Body>
-                  <Card.Title>{book.title}</Card.Title>
-                  <p className='small'>Authors: {book.authors}</p>
-                  <Card.Text>{book.description}</Card.Text>
-                  <Button className='btn-block btn-danger' onClick={() => handleDeleteBook(book.bookId)}>
-                    Delete this Book!
-                  </Button>
-                </Card.Body>
-              </Card>
-            );
-          })}
-        </CardColumns>
-      </Container>
-    </>
-  );
+	return (
+		<>
+			<Jumbotron fluid  className='search-panel' >
+				<Container>
+					<h2>{savedBooks.length
+						? `Total ${savedBooks.length}  ${
+								savedBooks.length === 1 ? 'record Found' : 'records found'
+						  }`
+						: 'No records found'}</h2>
+				</Container>
+			</Jumbotron>
+			<Container>
+				 
+				<CardColumns>
+					{savedBooks.map((book) => {
+						var description=book.description
+						return (
+							<Card key={book.bookId} className="shadow p-3 mb-5 bg-white rounded">
+								{book.image ? (
+									<Image className="mt-3 rounded mx-auto d-block"  src={book.image} alt={`The cover for ${book.title}`} /> 
+								) : null}
+								<Card.Body>
+									<Card.Title><LinesEllipsis
+									text={book.title}
+									maxLine='1'
+									ellipsis='...'
+									trimRight
+									basedOn='letters'
+									/></Card.Title>
+									<p className="small">Authors: {book.authors}</p>
+									<LinesEllipsis
+										text={description}
+										maxLine='3'
+										ellipsis='...'
+										trimRight
+										basedOn='letters'
+										/>
+									<Button
+										className="btn-block btn-danger"
+										onClick={() => handleDeleteBook(book.bookId)}
+									>
+										<i className="fa-solid fa-trash"></i> Remove Book
+									</Button>
+								</Card.Body>
+							</Card>
+						);
+					})}
+				</CardColumns>
+			</Container>
+		</>
+	);
 };
 
 export default SavedBooks;
--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -1,26 +1,29 @@
-import React, { useState, useEffect } from 'react';
-import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
-
-import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
-import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
-
+import React, { useState, useEffect } from "react";
+import {
+  Jumbotron,
+  Container,
+  Col,
+  Form,
+  Button,
+  Card,
+  CardColumns,
+} from "react-bootstrap";
+import Image from "react-bootstrap/Image";
+import Auth from "../utils/auth";
+import { SAVE_BOOK } from "../utils/mutations";
+import { searchGoogleBooks } from "../utils/API";
+import { saveBookIds, getSavedBookIds } from "../utils/localStorage";
+import { useMutation } from "@apollo/client";
+import LinesEllipsis from "react-lines-ellipsis";
 const SearchBooks = () => {
-  // create state for holding returned google api data
   const [searchedBooks, setSearchedBooks] = useState([]);
-  // create state for holding our search field data
-  const [searchInput, setSearchInput] = useState('');
-
-  // create state to hold saved bookId values
+  const [searchInput, setSearchInput] = useState("");
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
 
-  // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
-  // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
   useEffect(() => {
     return () => saveBookIds(savedBookIds);
   });
 
-  // create method to search for books and set state on form submit
   const handleFormSubmit = async (event) => {
     event.preventDefault();
 
@@ -32,46 +35,38 @@ const SearchBooks = () => {
       const response = await searchGoogleBooks(searchInput);
 
       if (!response.ok) {
-        throw new Error('something went wrong!');
+        throw new Error("something went wrong!");
       }
 
       const { items } = await response.json();
-
       const bookData = items.map((book) => ({
         bookId: book.id,
-        authors: book.volumeInfo.authors || ['No author to display'],
+        authors: book.volumeInfo.authors || ["No author to display"],
         title: book.volumeInfo.title,
         description: book.volumeInfo.description,
-        image: book.volumeInfo.imageLinks?.thumbnail || '',
+        image: book.volumeInfo.imageLinks?.thumbnail || "",
       }));
 
       setSearchedBooks(bookData);
-      setSearchInput('');
+      setSearchInput("");
     } catch (err) {
       console.error(err);
     }
   };
+  const [savedBook, { error }] = useMutation(SAVE_BOOK);
+  const handleSaveBook = async (searchedId) => {
+    const bookToSave = searchedBooks.find((book) => searchedId === book.bookId);
 
-  // create function to handle saving a book to our database
-  const handleSaveBook = async (bookId) => {
-    // find the book in `searchedBooks` state by the matching id
-    const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
-
-    // get token
     const token = Auth.loggedIn() ? Auth.getToken() : null;
-
     if (!token) {
       return false;
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
+      const book = await savedBook({
+        variables: { book: bookToSave },
+      });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
     } catch (err) {
       console.error(err);
@@ -80,24 +75,24 @@ const SearchBooks = () => {
 
   return (
     <>
-      <Jumbotron fluid className='text-light bg-dark'>
+      <Jumbotron fluid className="search-panel  ">
         <Container>
-          <h1>Search for Books!</h1>
           <Form onSubmit={handleFormSubmit}>
             <Form.Row>
               <Col xs={12} md={8}>
                 <Form.Control
-                  name='searchInput'
+                  name="searchInput"
                   value={searchInput}
                   onChange={(e) => setSearchInput(e.target.value)}
-                  type='text'
-                  size='lg'
-                  placeholder='Search for a book'
+                  type="text"
+                  className="mb-3"
+                  size="lg"
+                  placeholder="Enter Author Name or Book Title"
                 />
               </Col>
               <Col xs={12} md={4}>
-                <Button type='submit' variant='success' size='lg'>
-                  Submit Search
+                <Button type="submit" variant="primary" size="lg">
+                  <i className="fa-solid fa-magnifying-glass"></i> Search
                 </Button>
               </Col>
             </Form.Row>
@@ -106,30 +101,63 @@ const SearchBooks = () => {
       </Jumbotron>
 
       <Container>
-        <h2>
+        <h4 className="mb-3">
           {searchedBooks.length
-            ? `Viewing ${searchedBooks.length} results:`
-            : 'Search for a book to begin'}
-        </h2>
+            ? `Total ${searchedBooks.length} results found`
+            : ""}
+        </h4>
         <CardColumns>
           {searchedBooks.map((book) => {
+            var description = book.description;
+
+            //description=  description.length ;
             return (
-              <Card key={book.bookId} border='dark'>
+              <Card
+                key={book.bookId}
+                className="shadow p-3 mb-5 bg-white rounded"
+              >
                 {book.image ? (
-                  <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' />
+                  <Image
+                    className="mt-3 rounded mx-auto d-block"
+                    src={book.image}
+                    alt={`The cover for ${book.title}`}
+                  />
                 ) : null}
                 <Card.Body>
-                  <Card.Title>{book.title}</Card.Title>
-                  <p className='small'>Authors: {book.authors}</p>
-                  <Card.Text>{book.description}</Card.Text>
+                  <Card.Title>
+                    {" "}
+                    <LinesEllipsis
+                      text={book.title}
+                      maxLine="1"
+                      ellipsis="..."
+                      trimRight
+                      basedOn="letters"
+                    />
+                  </Card.Title>
+
+                  <p className="small">Authors: {book.authors}</p>
+                  <LinesEllipsis
+                    text={description}
+                    maxLine="3"
+                    ellipsis="..."
+                    trimRight
+                    basedOn="letters"
+                  />
+
                   {Auth.loggedIn() && (
                     <Button
-                      disabled={savedBookIds?.some((savedBookId) => savedBookId === book.bookId)}
-                      className='btn-block btn-info'
-                      onClick={() => handleSaveBook(book.bookId)}>
-                      {savedBookIds?.some((savedBookId) => savedBookId === book.bookId)
-                        ? 'This book has already been saved!'
-                        : 'Save this Book!'}
+                      disabled={savedBookIds?.some(
+                        (savedBookId) => savedBookId === book.bookId
+                      )}
+                      className="btn-block btn-success"
+                      onClick={() => handleSaveBook(book.bookId)}
+                    >
+                      <i className="fa-solid fa-floppy-disk"></i>
+                      {savedBookIds?.some(
+                        (savedBookId) => savedBookId === book.bookId
+                      )
+                        ? " This book has already been saved!"
+                        : " Save Book"}
                     </Button>
                   )}
                 </Card.Body>
@@ -137,6 +165,9 @@ const SearchBooks = () => {
             );
           })}
         </CardColumns>
+        {error && (
+          <div className="my-3 p-3 bg-danger text-white">{error.message}</div>
+        )}
       </Container>
     </>
   );
